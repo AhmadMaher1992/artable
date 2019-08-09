4fe3225d85827554b0085b46022a85cfd1bee0fe
Ahmad <eisamaherahmad@gmail.com>
2019-08-09 14:18:56 -0700

    Make Extension to String class which has isNotEmpty Attribute that return value equivalent to notEmpty And that make code more readable

---
+++ Shared/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Artable
+//
+//  Created by mac on 8/9/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var isNotEmpty: Bool{
+        return !isEmpty
+    }
+}
